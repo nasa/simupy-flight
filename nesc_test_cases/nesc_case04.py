8b48cf20d6823c403cb79bf7f3c1194528382155
Ben Margolis <benjamin.margoliss@nasa.gov>
2020-10-30 12:51:20 -0700

    remove unnecessary printing checks

--- nesc_test_cases/nesc_case04.py
+++ nesc_test_cases/nesc_case04.py
@@ -52,13 +52,6 @@ omega_Z_ic = 30.*np.pi/180
 
 planet.initial_condition = planet.ic_from_planetodetic(long_ic, lat_ic, h_ic, V_N_ic, V_E_ic, V_D_ic, psi_ic, theta_ic, phi_ic)
 planet.initial_condition[-3:] = omega_X_ic, omega_Y_ic, omega_Z_ic
-out_at_ic = planet.output_equation_function(0, planet.initial_condition)
-check_pos = out_at_ic[13:16]
-check_att = out_at_ic[16:19]
-orig_pos = np.array([long_ic, lat_ic, h_ic])
-orig_att = np.array([psi_ic, theta_ic, phi_ic])
-print('position:', np.allclose(check_pos, orig_pos))
-print('attitude:', np.allclose(check_att, orig_att))
 
 res = BD.simulate(30, integrator_options=int_opts)
 
