0f3435acfa855247f5da84d14a6c0663785db399
Kenneth Lyons <kenneth.r.lyons@nasa.gov>
2021-06-04 12:59:20 -0700

    Fix up test case runner

--- nesc_test_cases/run_nesc_cases.py
+++ nesc_test_cases/run_nesc_cases.py
@@ -1,22 +1,15 @@
 import os
+import sys
 from subprocess import call
-from nesc_testcase_helper import parser, save_relative_path
+from nesc_testcase_helper import nesc_options
 
 dirname = os.path.dirname(__file__)
 if dirname:
     os.chdir(dirname)
 
-if not os.path.exists(save_relative_path):
-    os.makedirs(save_relative_path)
-
-args = parser.parse_args()
-extra_flags = []
-if args.interactive:
-    extra_flags.append('--interactive')
-if args.simupy_scale:
-    extra_flags.append('--simupy-scale')
-if args.baseline05:
-    extra_flags.append('--baseline05')
+savepath = nesc_options['save_relative_path']
+if not os.path.exists(savepath):
+    os.makedirs(savepath)
 
 print("\n\n")
 cases_to_run = ["%02d" % num for num in range(1,12)]
@@ -25,6 +18,6 @@ cases_to_run += ["%02d" % num for num in range(15,17)]
 
 for case_id in cases_to_run:
     print("case number:", case_id)
-    call(["python", "nesc_case%s.py" % case_id] + extra_flags)
+    call(["python", "nesc_case%s.py" % case_id] + sys.argv[1:])
 
     print("\n\n")
